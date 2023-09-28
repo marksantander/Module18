9879d01aeda8b02445c6533ed0453f4c9d29878a
marksantander <msntndr.ms@gmail.com>
2023-09-27 20:42:37 -0400

    moved reactionSchema to top

--- models/users.js
+++ models/users.js
@@ -1,5 +1,30 @@
 const mongoose = require('mongoose');
 
+const reactionSchema = new mongoose.Schema({
+  reactionId: {
+    type: mongoose.Schema.Types.ObjectId,
+    default: new mongoose.Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: function (timestamp) {
+      return timestamp.toLocaleDateString();
+    },
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
@@ -48,28 +73,4 @@ thoughtSchema.virtual('reactionCount').get(function () {
   return this.reactions.length;
 });
 
-const reactionSchema = new mongoose.Schema({
-  reactionId: {
-    type: mongoose.Schema.Types.ObjectId,
-    default: new mongoose.Types.ObjectId(),
-  },
-  reactionBody: {
-    type: String,
-    required: true,
-    maxlength: 280,
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: function (timestamp) {
-      return timestamp.toLocaleDateString();
-    },
-  },
-});
-
-const User = mongoose.model('User', userSchema);
 const Thought = mongoose.model('Thought', thoughtSchema);
